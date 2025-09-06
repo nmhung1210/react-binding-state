9fb776eb43b8103d56c5ccb03d56dac72b321282
nmhung1210 <nmhung1210@gmail.com>
2025-09-06 15:12:09 +0000

    Changes before Firebase Studio auto-run

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  external: ['react', 'react-dom', 'prop-types'],
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+    }),
+    terser(),
+  ],
+};
