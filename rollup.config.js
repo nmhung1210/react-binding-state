6e61e17d27538f986dc53c3bfb6c481d30d07e77
nmhung1210 <nmhung1210@gmail.com>
2025-09-06 15:18:59 +0000

    Adding rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default {
   input: 'src/index.js',
@@ -8,6 +9,7 @@ export default {
       file: 'dist/index.cjs.js',
       format: 'cjs',
       sourcemap: true,
+      exports: 'auto',
     },
     {
       file: 'dist/index.esm.js',
@@ -15,8 +17,12 @@ export default {
       sourcemap: true,
     },
   ],
-  external: ['react', 'react-dom', 'prop-types'],
+  external: ['react', 'react-dom'],
   plugins: [
+    // This teaches Rollup how to find modules and resolve .jsx files.
+    nodeResolve({
+      extensions: ['.js', '.jsx'],
+    }),
     babel({
       exclude: 'node_modules/**',
       presets: ['@babel/preset-env', '@babel/preset-react'],
